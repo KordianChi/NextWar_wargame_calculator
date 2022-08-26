f7f6e35279de0e8004164cba60f3ff1025bae8d3
KordianChi <kordianczyzewski@gmail.com>
2022-08-26 13:11:39 +0200

    SOF Table ready

--- SOF_Table.py
+++ SOF_Table.py
@@ -150,13 +150,91 @@ class MyWindow:
         self.result_ent.delete(0, 'end')
 
         d10_result = randint(0, 9)
-        if self.mission_type.get() == 0 and self.recon_mission_target_cbx.get() == 'Targeting':
-            targeting_result = ['T', 'T', 'T', 'T', 'T', '-', '-', '-', '-']
+        if self.mission_type.get() == 0:
             row = d10_result
             if row > 8:
                 row = 8
-            result = targeting_result[row]
+            if self.recon_mission_target_cbx.get() == 'Targeting':
+                targeting_result = ['T', 'T', 'T', 'T', 'T', '-', '-', '-', '-']
+                result = targeting_result[row]
+            else:
+                hq_recon_target = {'Flat/Rough/Marsh': 3, 'Flat Woods/Rough Wds': 2,
+                                   'Highland/Highland Wds': 1, 'Mountain/Urban/Jungle': 0}
+                sam_recon_target = {'Flat/Rough/Marsh': 3, 'Flat Woods/Rough Wds': 2,
+                                    'Highland/Highland Wds': 1, 'Mountain/Urban/Jungle': 0}
+                depot_recon_target = {'Flat/Rough/Marsh': 4, 'Flat Woods/Rough Wds': 3,
+                                      'Highland/Highland Wds': 2, 'Mountain/Urban/Jungle': 1}
+                msu_recon_target = {'Flat/Rough/Marsh': 5, 'Flat Woods/Rough Wds': 4,
+                                    'Highland/Highland Wds': 3, 'Mountain/Urban/Jungle': 2}
+                ground_unit_recon_target = {'Flat/Rough/Marsh': 5, 'Flat Woods/Rough Wds': 4,
+                                            'Highland/Highland Wds': 3, 'Mountain/Urban/Jungle': 2}
+                recon_table = {'HQ': hq_recon_target, 'SAM': sam_recon_target, 'Supply Depot': depot_recon_target,
+                               'MSU': msu_recon_target, 'Ground Unit': ground_unit_recon_target}
+                target = recon_table[self.recon_mission_target_cbx.get()]
+                column = target[self.terrain_type_cbx.get()]
+                recon_result_table = [['D', 'D', '-', '-', '-', '-', '-', '-', '-'],
+                                      ['D', 'D', 'D', '-', '-', '-', '-', '-', '-'],
+                                      ['D', 'D', 'D', 'D', '-', '-', '-', '-', '-'],
+                                      ['D', 'D', 'D', 'D', 'D', '-', '-', '-', '-'],
+                                      ['D', 'D', 'D', 'D', 'D', 'D', '-', '-', '-'],
+                                      ['D', 'D', 'D', 'D', 'D', 'D', 'D', '-', '-']]
+                result = recon_result_table[column][row]
 
+        else:
+            drm_raid = 0
+            if self.storm.get():
+                drm_raid -= 1
+            if self.bridge.get():
+                drm_raid -= 1
+            if self.city.get():
+                drm_raid -= 1
+            if self.naval.get():
+                drm_raid += 1
+            if self.less_brigade.get():
+                drm_raid += 1
+            if self.least_brigade.get():
+                drm_raid += 2
+            if self.sam_or_theater.get():
+                drm_raid += 1
+            row = d10_result + drm_raid
+            if row < 0:
+                row = 0
+            if row > 8:
+                row = 8
+            if self.raid_mission_target_cbx.get() == 'Detection/SAM/Theater Weapon':
+                detection_result = ['-2', '-2', '-1', '-1', '-1', '-', '-', '-', '-']
+                result = detection_result[row]
+            else:
+                hq_raid_target = {'Flat/Rough/Marsh': 0, 'Flat Woods/Rough Wds': 1,
+                                  'Highland/Highland Wds': 2, 'Mountain/Urban/Jungle': 3}
+                depot_raid_target = {'Flat/Rough/Marsh': 0, 'Flat Woods/Rough Wds': 1,
+                                     'Highland/Highland Wds': 2, 'Mountain/Urban/Jungle': 3}
+                msu_raid_target = {'Flat/Rough/Marsh': 3, 'Flat Woods/Rough Wds': 4,
+                                   'Highland/Highland Wds': 5, 'Mountain/Urban/Jungle': 6}
+                naval_raid_target = {'Flat/Rough/Marsh': 1, 'Flat Woods/Rough Wds': 2,
+                                     'Highland/Highland Wds': 3, 'Mountain/Urban/Jungle': 4}
+                helo_raid_target = {'Flat/Rough/Marsh': 2, 'Flat Woods/Rough Wds': 3,
+                                    'Highland/Highland Wds': 4, 'Mountain/Urban/Jungle': 5}
+                airfield_raid_target = {'Flat/Rough/Marsh': 2, 'Flat Woods/Rough Wds': 3,
+                                        'Highland/Highland Wds': 4, 'Mountain/Urban/Jungle': 5}
+                instalation_raid_target = {'Flat/Rough/Marsh': 1, 'Flat Woods/Rough Wds': 2,
+                                           'Highland/Highland Wds': 3, 'Mountain/Urban/Jungle': 4}
+                interdiction_raid_target = {'Flat/Rough/Marsh': 7, 'Flat Woods/Rough Wds': 0,
+                                            'Highland/Highland Wds': 1, 'Mountain/Urban/Jungle': 2}
+                raid_table = {'HQ': hq_raid_target, 'Supply Depot': depot_raid_target, 'MSU': msu_raid_target,
+                              'Naval': naval_raid_target, 'Airfield': airfield_raid_target, 'Helo': helo_raid_target,
+                              'Instalation': instalation_raid_target, 'Interdiction': interdiction_raid_target}
+                target = raid_table[self.raid_mission_target_cbx.get()]
+                column = target[self.terrain_type_cbx.get()]
+                raid_result_table = [['1', '1', '-', '-', '-', '-', '-', '-', '-'],
+                                     ['2', '1', '1', '-', '-', '-', '-', '-', '-'],
+                                     ['2', '2', '1', '1', '-', '-', '-', '-', '-'],
+                                     ['X', '2', '2', '1', '1', '-', '-', '-', '-'],
+                                     ['X', 'X', '2', '2', '1', '1', '-', '-', '-'],
+                                     ['X', 'X', 'X', '2', '2', '1', '1', '-', '-'],
+                                     ['X', 'X', 'X', 'X', '2', '2', '1', '1', '-'],
+                                     ['-', '-', '-', '-', '-', '-', '-', '-', '-']]
+                result = raid_result_table[column][row]
 
 # Check survive
         d10_survive = randint(0, 9)
