2d8f9eb2157eefec36a6a8a8df2708d4c43df9a8
KordianChi <kordianczyzewski@gmail.com>
2022-10-06 13:31:24 +0200

    supply and electronic detection calculator

--- electronic_detection.py
+++ electronic_detection.py
@@ -1,4 +1,4 @@
-from tkinter import Label, BooleanVar, Toplevel
+from tkinter import Label, Toplevel, Radiobutton, IntVar, BooleanVar
 from tkinter import Entry
 from tkinter import Button
 from tkinter import END
@@ -10,4 +10,36 @@ class ElectronicDetection(Toplevel):
 
         super().__init__(parent)
         self.title('Electronic Detection')
-        self.geometry("300x270+10+10")
+        self.geometry("280x140+10+10")
+
+        self.side = BooleanVar()
+        self.side.set(True)
+
+        self.non_allied_r = Radiobutton(self, text='Non-allied', variable=self.side, value=False)
+        self.allied_r = Radiobutton(self, text='Allied', variable=self.side, value=True)
+
+        self.result_btn = Button(self, text='Detection', command=self.detection_result)
+        self.result_lbl = Label(self, text='Det:')
+        self.result_ent = Entry(self, width=5)
+
+        self.result_btn.place(x=20, y=70)
+        self.result_ent.place(x=60, y=100)
+        self.result_lbl.place(x=20, y=100)
+        self.non_allied_r.place(x=20, y=20)
+        self.allied_r.place(x=20, y=40)
+
+    def detection_result(self):
+        self.result_ent.delete(0, 'end')
+
+        d10 = randint(0, 9)
+
+        result = '-'
+
+        if self.side.get():
+            if d10 < 5:
+                result = 'D'
+        else:
+            if d10 < 3:
+                result = 'D'
+
+        self.result_ent.insert(END, result)
--- supply.py
+++ supply.py
@@ -1,4 +1,4 @@
-from tkinter import Label, BooleanVar, Toplevel
+from tkinter import Label, BooleanVar, Toplevel, Radiobutton
 from tkinter import Entry
 from tkinter import Button
 from tkinter import END
@@ -10,4 +10,36 @@ class Supply(Toplevel):
 
         super().__init__(parent)
         self.title('Supply')
-        self.geometry("300x270+10+10")
+        self.geometry("200x100+10+10")
+
+        self.supply_btn = Button(self, text='Supply', command=self.supply_result)
+        self.supply_lbl = Label(self, text='Result:')
+        self.supply_ent = Entry(self, width=7)
+
+        self.unit = BooleanVar()
+        self.unit.set(True)
+
+        self.hq_r = Radiobutton(self, text='HQ', variable=self.unit, value=False)
+        self.ground_unit_r = Radiobutton(self, text='Unit', variable=self.unit, value=True)
+
+        self.supply_btn.place(x=20, y=20)
+        self.hq_r.place(x=70, y=20)
+        self.ground_unit_r.place(x=120, y=20)
+        self.supply_lbl.place(x=20, y=50)
+        self.supply_ent.place(x=70, y=50)
+
+    def supply_result(self):
+
+        self.supply_ent.delete(0, 'end')
+
+        result = '-'
+        d10 = randint(0, 9)
+
+        if self.unit:
+            if d10 < 6:
+                result = 'Supply'
+        else:
+            if d10 < 3:
+                result = 'Supply'
+
+        self.supply_ent.insert(END, result)
