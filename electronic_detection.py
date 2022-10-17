5f6eaa94829282ca647d2bc4b31024d6ced9c4a4
KordianChi <kordianczyzewski@gmail.com>
2022-10-17 10:03:14 +0200

    add cyberattack option for detection

--- electronic_detection.py
+++ electronic_detection.py
@@ -1,4 +1,4 @@
-from tkinter import Label, Toplevel, Radiobutton, IntVar, BooleanVar
+from tkinter import Label, Toplevel, Radiobutton, BooleanVar
 from tkinter import Entry
 from tkinter import Button
 from tkinter import END
@@ -18,6 +18,14 @@ class ElectronicDetection(Toplevel):
         self.non_allied_r = Radiobutton(self, text='Non-allied', variable=self.side, value=False)
         self.allied_r = Radiobutton(self, text='Allied', variable=self.side, value=True)
 
+        self.non_allied_cyber = BooleanVar()
+        self.non_allied_cyber.set(False)
+        self.non_allied_cyber_chk = Checkbutton(self, text='Non-allied cyberattack', variable=self.non_allied_cyber)
+
+        self.allied_cyber = BooleanVar()
+        self.allied_cyber.set(False)
+        self.allied_cyber_chk = Checkbutton(self, text='Allied cyberattack', variable=self.allied_cyber)
+
         self.result_btn = Button(self, text='Detection', command=self.detection_result)
         self.result_lbl = Label(self, text='Det:')
         self.result_ent = Entry(self, width=5)
@@ -27,6 +35,8 @@ class ElectronicDetection(Toplevel):
         self.result_lbl.place(x=20, y=100)
         self.non_allied_r.place(x=20, y=20)
         self.allied_r.place(x=20, y=40)
+        self.allied_cyber_chk.place(x=110, y=20)
+        self.non_allied_cyber_chk.place(x=110, y=40)
 
     def detection_result(self):
         self.result_ent.delete(0, 'end')
@@ -36,9 +46,17 @@ class ElectronicDetection(Toplevel):
         result = '-'
 
         if self.side.get():
+            if self.allied_cyber.get():
+                d10 -= 2
+            if self.non_allied_cyber.get():
+                d10 += 2
             if d10 < 5:
                 result = 'D'
         else:
+            if self.allied_cyber.get():
+                d10 += 2
+            if self.non_allied_cyber.get():
+                d10 -= 2
             if d10 < 3:
                 result = 'D'
 
